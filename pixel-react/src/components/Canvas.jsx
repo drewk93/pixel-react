af2ad2a92e4ac0d4c25f3472ed9cf62ac7df5e1d
Drew Kullman <agkullman@protonmail.com>
2024-08-20 19:12:51 -0600

    Added canvas, and basic styling

---
+++ pixel-react/src/components/Canvas.jsx
@@ -0,0 +1,11 @@
+ const Canvas = () => {
+    return (
+        <section id="canvas-component">
+            <div id="canvas-area">
+
+            </div>
+        </section>
+    )
+}
+
+export default Canvas;
