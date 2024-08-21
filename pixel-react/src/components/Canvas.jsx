8b56ffb2d7b9568c2c87103dddf49db1956b33f6
Drew Kullman <agkullman@protonmail.com>
2024-08-21 12:06:31 -0600

    Added pixels to project, will reconfigure for better event-handling and color delegation

--- pixel-react/src/components/Canvas.jsx
+++ pixel-react/src/components/Canvas.jsx
@@ -1,8 +1,19 @@
+ import Pixels from './Pixels'
+ import {useState, useEffect} from 'react';
+ 
  const Canvas = () => {
+
+    const colors = ['black']
+    const [currentColor, setCurrentColor] = useState(colors[0]);
+    const [pixelData, setPixelData] = useState(10000);
+    const colorPixel = (currentColor) => {
+        
+    }
+
     return (
         <section id="canvas-component">
             <div id="canvas-area">
-
+                <Pixels pixelData={pixelData} setPixelData={setPixelData} />
             </div>
         </section>
     )
---
+++ pixel-react/src/components/Pixels.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const Pixels = ({ pixelData, setPixelData }) => {
+    // Create an array with pixelData number of elements and initial color for each pixel
+    const initialColors = Array.from({ length: pixelData }, () => 'white');
+    const [colors, setColors] = useState(initialColors);
+
+    // Handle click event to change the color of the clicked pixel
+    const handleClick = (index) => {
+        // Create a new colors array with the updated color for the clicked pixel
+        const newColors = [...colors];
+        newColors[index] = 'black'; // Change color to black on click
+        setColors(newColors);
+
+        // Optionally, you might want to update pixelData or perform other actions here
+        // setPixelData(...);
+    };
+
+    return (
+        <>
+            {colors.map((color, index) => (
+                <div
+                    key={index}
+                    className="pixel"
+                    style={{ backgroundColor: color }} // Set background color for each pixel
+                    onClick={() => handleClick(index)} // Handle click event
+                >
+                    {/* Render content for each pixel here */}
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default Pixels;
+
+
+//Consider using this
+
+// const MyComponent = () => {
+//   const size = 10;
+//   const numArray = [];
+
+//   for (let i = 0; i < size; i++) {
+//     numArray.push(size - i);
+//   }
+
+//   return (
+//     <div>
+//       {numArray.map((num, index) => (
+//         <div key={index}>Item {num}</div>
+//       ))}
+//     </div>
+//   );
+// };
